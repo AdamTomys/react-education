39c17dbea8896795b7dc2159cb7d361448856d41
Adam Tomys <adamtomys@gmail.com>
2020-12-29 17:14:58 +0100

    deployment

--- src/redux/searchStringRedux.js
+++ src/redux/searchStringRedux.js
@@ -7,6 +7,8 @@ export const countAllCards = ({cards}) => cards.length;
 export const countVisibleCards = ({cards, searchString}) => 
   cards.filter((card) => new RegExp(searchString, 'i').test(card.title)).length;
 
+// alternatywna metoda bez uzycia specjalnych bibliotek.
+
 // export function countVisibleCards({cards, searchString}) {
 //   const array = [];
 //   for(let card of cards) {
@@ -14,9 +16,26 @@ export const countVisibleCards = ({cards, searchString}) =>
 //       array.push(card);
 //     }
 //   }
+//   return array.length;
 // }
 
+const reducerName = 'searchString';
+const createActionName = (name) => `app/${reducerName}/${name}`;
+
+export const SEARCH_CARD = createActionName('SEARCH_CARD');
+
+export const createActionSearchCard = (payload) => ({
+  payload: payload, 
+  id: shortid.generate(),
+  type: SEARCH_CARD,
+});
+
 // reducer
-export default function reducer(statePart = '') {
-  return statePart;
+export default function reducer(statePart = '', action = '') {
+  switch (action.type) {
+    case SEARCH_CARD:
+      return action.payload;
+    default:
+      return statePart;
+  }
 }
