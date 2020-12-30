5b441d5d26f7c3fc129eb63a8090e3a30c37b4a0
Adam Tomys <adamtomys@gmail.com>
2020-12-30 21:03:02 +0100

    cards movement

--- src/redux/cardsRedux.js
+++ src/redux/cardsRedux.js
@@ -25,8 +25,52 @@ export default function reducer(state = [], action = {}) {
       return [...state, action.payload];
     case MOVE_CARD: {
       const {id, src, dest} = action.payload;
-      console.log(id, src, dest);
-      return state;
+      const targetCard = state.filter(card => card.id === id)[0];
+      const targetColumnCards = state.filter(card => card.columnId == dest.columnId).sort((a, b) => a.index - b.index);
+      if(dest.columnId == src.columnId) {
+        targetColumnCards.splice(src.index, 1);
+        targetColumnCards.splice(dest.index, 0, targetCard);
+        return state.map(card => {
+          const targetColumnIndex = targetColumnCards.indexOf(card);
+          if(targetColumnIndex > -1 && card.index != targetColumnIndex) {
+            console.log({...card, index: targetColumnIndex});
+            return {...card, index: targetColumnIndex};
+          } else {
+            return card;
+          }
+        });
+      } else {
+        let sourceColumnCards = state.filter(card => card.columnId == src.columnId).sort((a, b) => a.index - b.index);
+
+        // remove card from sourceColumn
+        sourceColumnCards.splice(src.index, 1);
+        // add card to targetColumn
+        targetColumnCards.splice(dest.index, 0, targetCard);
+
+        return state.map(card => {
+          const targetColumnIndex = targetColumnCards.indexOf(card);
+
+          if(card == targetCard){
+            // card is targetCard
+            return {...card, index: targetColumnIndex, columnId: dest.columnId};
+          } else if(targetColumnIndex > -1 && card.index != targetColumnIndex){
+            // card is in targetColumn
+            console.log({...card, index: targetColumnIndex});
+            return {...card, index: targetColumnIndex};
+          } else {
+            // card is NOT in targetColumn
+            const sourceColumnIndex = sourceColumnCards.indexOf(card);
+
+            if(sourceColumnIndex > -1 && card.index != sourceColumnIndex){
+              // card is in sourceColumn
+              return {...card, index: sourceColumnIndex};
+            } else {
+              // card is NOT in sourceColumn (and NOT in targetColumn)
+              return card;
+            }
+          }
+        });
+      }
     }
     default:
       return state;
